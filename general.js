5d7fe6b18127139dc9415b9a7697623f8911b68d
TruongTQ261531 <tranminhtruong271105@gmail.com>
2026-07-01 13:52:37 +0700

    Implement user registration and book retrieval routes

---
+++ general.js
@@ -0,0 +1,63 @@
+const express = require('express');
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
+const axios = require('axios');
+
+public_users.post("/register", (req,res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    if (!isValid(username)) {
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "Customer successfully registered. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});
+    }
+  }
+  return res.status(404).json({message: "Unable to register user."});
+});
+
+// Get the book list available in the shop
+public_users.get('/',function (req, res) {
+  res.send(JSON.stringify(books,null,4));
+});
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
+ });
+  
+// Get book details based on author
+public_users.get('/author/:author',function (req, res) {
+  const author = req.params.author;
+  let output = {};
+  for (let isbn in books) {
+    if (books[isbn].author === author) {
+      output[isbn] = { "title": books[isbn].title, "reviews": books[isbn].reviews };
+    }
+  }
+  res.send(output);
+});
+
+// Get all books based on title
+public_users.get('/title/:title',function (req, res) {
+  const title = req.params.title;
+  let output = {};
+  for (let isbn in books) {
+    if (books[isbn].title === title) {
+      output[isbn] = { "author": books[isbn].author, "reviews": books[isbn].reviews };
+    }
+  }
+  res.send(output);
+});
+
+//  Get book review
+public_users.get('/review/:isbn',function (req, res) {
+  const isbn = req.params.isbn;
+  res.send(books[isbn].reviews);
+});
+
+module.exports.general = public_users;
